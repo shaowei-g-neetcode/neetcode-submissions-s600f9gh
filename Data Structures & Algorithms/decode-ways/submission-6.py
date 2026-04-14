c0f6852c7792c641715543768b4be889df0130b3
shaowei-g-neetcode <waouo04@gmail.com>
2026-04-14 16:44:36 +0800

    Update: decode-ways - submission-6

--- Data Structures & Algorithms/decode-ways/submission-6.py
+++ Data Structures & Algorithms/decode-ways/submission-6.py
@@ -1,21 +1,18 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-      if s[0] == '0':
-        return 0
+        # state dp[i] = dp[i:] means there are how many ways there
+        # basecase dp[n] = 1
+        # transition single valid char: dp[i] += dp[i + 1]
+        # twice valid char: dp[i] += dp[i + 2]
+        n = len(s)
 
-      n = len(s)
+        dp = [0] * (n+1)
+        dp[n] = 1
 
-      dp = [0] * (n + 1)
-      dp[0] = 1
-      dp[1] = 1
+        for i in range(n - 1, -1, -1):
+            if s[i] != '0':
+                dp[i] += dp[i + 1]
+            if i + 1 < n  and '10' <= s[i:i+2] <= '26':
+                dp[i] += dp[i + 2]
 
-      for i in range(2, n + 1):
-        if s[i-1] != '0':
-          dp[i] += dp[i-1]
-        if '10'<=(s[i-2:i])<='26':
-          dp[i] += dp[i-2]
-      
-      return dp[n]
-           
-        
-        
+        return dp[0]
