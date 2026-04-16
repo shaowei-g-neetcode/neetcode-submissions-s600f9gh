3b04fb9227d3c32bc45a675d80c51b6798bf4e2a
shaowei-g-neetcode <waouo04@gmail.com>
2026-04-16 15:23:47 +0800

    Add: rotate-matrix - submission-2

---
+++ Data Structures & Algorithms/rotate-matrix/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+
+        n = len(matrix)
+        
+        # swap top bottom 
+        for top in range(n//2):
+            bottom = n - 1 + top
+            matrix[top], matrix[bottom] = matrix[bottom], matrix[top]
+        
+        # transpose
+        for i in range(n):
+            for j in range(i+1, n):  
+                matrix[i][j], matrix[j][i] = matrix[j][i],matrix[i][j]
+
