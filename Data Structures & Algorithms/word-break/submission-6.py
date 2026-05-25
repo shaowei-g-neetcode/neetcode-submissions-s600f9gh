b4565771535b7327ad842fa8c1e9a49e6e44e263
shaowei-g-neetcode <waouo04@gmail.com>
2026-05-25 11:13:03 +0800

    Add: word-break - submission-6

---
+++ Data Structures & Algorithms/word-break/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        # dp[i] means s[j:i] can break to words
+        # trans:
+        # dp[i] = s[j] == true and s[j:i] in words
+        # base: dp[0] = true
+        n = len(s) + 1
+        dp = [False] * n
+        dp[0] = True
+        words = set(wordDict)
+
+        for i in range(1, n):
+            for j in range(i):
+                if dp[j] == True and s[j:i] in words:
+                    dp[i] = True
+                    break
+        
+        return dp[-1]
