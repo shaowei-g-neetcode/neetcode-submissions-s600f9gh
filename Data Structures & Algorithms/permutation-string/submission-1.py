7cbefc77de5f469aa5073e9667a68a1f18ea1e73
shaowei-g-neetcode <waouo04@gmail.com>
2026-05-25 16:33:55 +0800

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # use sliding window to compare s1 need list and s2 window
+        m, n = len(s1), len(s2)
+        if m > n:
+            return False
+
+        need = [0] * 26
+        window = [0] * 26
+
+        def getLetterInx(s:str):
+            return ord(s) - ord('a')
+
+        for i in range(m):
+            need[getLetterInx(s1[i])] += 1
+            window[getLetterInx(s2[i])] += 1
+
+        if need == window:
+            return True
+        
+        for right in range(m, n):
+            window[getLetterInx(s2[right])] += 1
+            window[getLetterInx(s2[right - m ])] -= 1
+
+            if window == need:
+                return True
+        return False
