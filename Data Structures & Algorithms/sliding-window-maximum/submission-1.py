006dac88d669757a3132dc18a3b9f548bd09dea3
shaowei-g-neetcode <waouo04@gmail.com>
2026-05-27 14:45:19 +0800

    Add: sliding-window-maximum - submission-1

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.py
@@ -0,0 +1,23 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        # use slide window and monotonic queue to save max num index
+        q = deque()
+        res = []
+
+        for i, num in enumerate(nums):
+            # i - k + 1 is left boundary
+            # remove left num of left boundary
+            while q and q[0] < i - k + 1:
+                q.popleft()
+            
+            # remove candidates that can no longer become maximum
+            while q and nums[i] >= nums[q[-1]]:
+                q.pop()
+            
+            q.append(i)
+
+            # append max num
+            if i - k  + 1 >=0:
+                res.append(nums[q[0]])
+
+        return res
