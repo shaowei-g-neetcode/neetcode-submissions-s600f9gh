16898b353ac52b452e7d6987703059d3fa7dda04
shaowei-g-neetcode <waouo04@gmail.com>
2026-05-29 10:47:49 +0800

    Add: permutation-string - submission-6

---
+++ Data Structures & Algorithms/permutation-string/submission-6.py
@@ -0,0 +1,29 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # use two window to compare theirs char counts
+
+        need = [0] * 26
+        window = [0] * 26
+        m, n = len(s1), len(s2)
+
+        if m > n:
+            return False
+
+        def getCharIdx(s: str):
+            return ord(s) - ord('a')
+
+        for i in range(m):
+            need[getCharIdx(s1[i])] += 1
+            window[getCharIdx(s2[i])] += 1
+        
+        if need == window:
+            return True
+
+        for right in range(m, n):
+            window[getCharIdx(s2[right])] += 1
+            window[getCharIdx(s2[right - m])] -= 1
+
+            if need == window:
+                return True
+
+        return False
