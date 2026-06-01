39044753b2b05409fcec309a6841adeda15ec91f
shaowei-g-neetcode <waouo04@gmail.com>
2026-06-01 15:21:33 +0800

    Add: decode-ways - submission-9

---
+++ Data Structures & Algorithms/decode-ways/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        # dp[i] means how many ways to decode string
+        # trans:
+        # single char: dp[i] += dp[i-1]
+        # two char: dp[i] += dp[i-2]
+        # base: dp[0] = 1
+
+        n = len(s) + 1
+        dp = [0] * n
+        dp[0] = 1
+
+        for i in range(1, n):
+            if s[i-1] != '0':
+                dp[i] += dp[i-1]
+            if i >= 2 and 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+            
+        return dp[-1]
