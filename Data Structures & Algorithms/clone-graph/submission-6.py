bff16247f577b4f27d92196686f9992615fbc9a2
shaowei-g-neetcode <waouo04@gmail.com>
2026-06-16 09:59:10 +0800

    Add: clone-graph - submission-6

---
+++ Data Structures & Algorithms/clone-graph/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def cloneGraph(self, node: Optional["Node"]) -> Optional["Node"]:
+        if not node:
+            return None
+
+        cloneMap = {}
+
+        def dfs(cur):
+            if cur in cloneMap:
+                return cloneMap[cur]
+            copy = Node(cur.val)
+            cloneMap[cur] = copy
+
+            for nei in cur.neighbors:
+                copy.neighbors.append(dfs(nei)) 
+            return copy
+        
+        return dfs(node)
