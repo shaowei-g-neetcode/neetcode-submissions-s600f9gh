74493ee5a851952e3ab92b9e48fff7eccf870eac
shaowei-g-neetcode <waouo04@gmail.com>
2026-06-22 15:05:17 +0800

    Add: minimum-window-with-characters - submission-5

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-5.py
@@ -0,0 +1,29 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        # use two window
+        # use windowC and needC to count chars in s and t
+        # have and need represent the counts of windowC[c] == needC[c]
+        
+        windowC, needC = {}, {}
+
+        for c in t:
+            needC[c] = needC.get(c, 0) + 1
+        
+        have, need = 0, len(needC)
+        l = 0
+        resIdx , resLen = -1, float("inf")
+        for r in range(len(s)):
+            windowC[s[r]] = windowC.get(s[r], 0) + 1
+
+            if s[r] in needC and windowC[s[r]] == needC[s[r]]:
+                have += 1
+            while have == need:
+                if r - l + 1 < resLen:
+                    resIdx = l
+                    resLen = r - l + 1
+                windowC[s[l]] -= 1
+                if s[l] in needC and windowC[s[l]] < needC[s[l]]:
+                    have -= 1
+                l += 1                
+        return s[resIdx: resIdx + resLen] if resIdx != -1 else ""
+
