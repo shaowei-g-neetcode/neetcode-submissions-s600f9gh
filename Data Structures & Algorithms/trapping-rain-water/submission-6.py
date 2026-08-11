7ce51aa2d29d685c5c86ae0c3480b250096f9513
shaowei-g-neetcode <waouo04@gmail.com>
2026-08-11 16:09:23 +0800

    Add: trapping-rain-water - submission-6

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-6.py
@@ -0,0 +1,30 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        # use two pointers to calc water
+        # vars: l, r, lMax, rMax
+        # for next right i posistion
+        # if rMax >= lMax
+        # it's water deside by lMax
+        # water = lMax - height[l] 
+        # else
+        # water = rMax - height[r]
+        # how to move pointers?
+        # Move the side with smarller max boundary
+        n = len(height)
+        if n < 3:
+            return 0
+        l, r = 0, n - 1
+        lMax, rMax = height[l], height[r]
+        water = 0
+
+        while l < r:
+            if rMax >= lMax:
+                l += 1
+                lMax = max(lMax, height[l]) 
+                water += lMax  - height[l]
+            else:
+                r -= 1
+                rMax = max(rMax, height[r])
+                water += rMax - height[r]
+
+        return water    
